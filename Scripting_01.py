e6604df2c70c290cbe53eacd19e601650b5cbf8f
gaurav-ace <gaurav7101879@gmail.com>
2023-01-19 16:38:49 +0530

    first commit : created a script for organizing files/folders in a directory.

---
+++ Scripting_01.py
@@ -0,0 +1,50 @@
+
+#first python script
+
+#print('testing python scripting...')
+
+
+#script for arranging the files of a folder as per extensions of files
+
+# take all files in a folder
+# annotate respective category based on the extension
+
+import os
+from pathlib import Path
+subdirectory = {
+    'Videos' : ['.mp4', '.3gp', '.avi', '.mov'],
+    'Pictures' : ['.jpg', '.jpeg', '.png'],
+    'Documents' : ['.txt', '.pdf', '.docx'],
+    'Audio' : ['.m4a', '.m4b', '.mp3']
+}
+
+def findFolder(extension):
+    for key,values in subdirectory.items():
+        for value in values:
+            if value == extension:
+                return key
+    return 'MISC'
+
+#testing the findFolder functionality -
+#print(findFolder('.pdf'))
+
+def OrganizeDir():
+    for item in os.scandir():
+        if item.is_dir():
+            continue
+
+        filePath = Path(item)
+        #print(filePath)
+
+        #print(filePath.suffix)
+        ext = filePath.suffix.lower()
+        folder  = findFolder(ext)
+        folderPath = Path(folder)
+        if folderPath.is_dir() != True:
+            folderPath.mkdir()
+
+        filePath.rename(folderPath.joinpath(filePath))    
+
+
+OrganizeDir()
+    
